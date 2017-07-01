be12e8729f0a55a162019c9f20216d27c5e646fe
laoheihei <studylalala@163.com>
2017-07-01 14:50:29 +0800

    Initial commit

---
+++ src/com/heihei/test/DemoTest.java
@@ -0,0 +1,10 @@
+package com.heihei.test;
+
+/**
+ * Created by Administrator on 2017/7/1.
+ */
+public class DemoTest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
